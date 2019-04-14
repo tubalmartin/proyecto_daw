12a70154eae2439ebdf3eca64a4ec85e78ee8418
tubalmartin <tubalmartin@gmail.com>
2019-04-14 21:15:11 +0200

    Initial setup

---
+++ application/views/base.php
@@ -0,0 +1,7 @@
+<?php
+
+$this->load->view('html_start');
+$this->load->view('header');
+$this->load->view($view);
+$this->load->view('footer');
+$this->load->view('html_end');
---
+++ application/views/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tubal
+ * Date: 2019-04-14
+ * Time: 20:57
+ */
