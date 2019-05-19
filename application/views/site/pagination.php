eeed29e70853447afa2a911de8622a1cf6f03919
tubalmartin <tubalmartin@gmail.com>
2019-05-19 23:52:13 +0200

    Public site zone done (added missing uncommitted files)

---
+++ application/views/site/pagination.php
@@ -0,0 +1,5 @@
+<div class="row mt-3">
+    <div class="col text-center">
+        <?php echo $this->pagination->create_links(); ?>
+    </div>
+</div>
---
+++ application/views/site/search_form.php
@@ -0,0 +1,5 @@
+<div class="row mb-4">
+    <div class="col">
+        <?php $this->load->view('common/search_form', $data); ?>
+    </div>
+</div>
